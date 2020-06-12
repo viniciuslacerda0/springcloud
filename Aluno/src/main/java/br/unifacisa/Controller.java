82221c3b1ba6893b70b1c248e35fc1f6ab5eb398
Hiago Marques <hiagomarquesdemedeiros@gmail.com>
2020-06-12 00:55:44 -0300

    remove config

--- Aluno/src/main/java/br/unifacisa/Controller.java
+++ Aluno/src/main/java/br/unifacisa/Controller.java
@@ -1,7 +1,6 @@
 package br.unifacisa;
 
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 	
-	@Value("${config.value}")
-	private String value;
 	
 	@GetMapping
 	public ResponseEntity<String> get() {
-		return ResponseEntity.ok("Achei aluno " + value);
+		return ResponseEntity.ok("Achei aluno ");
 	}
 }
--- GateWay/src/main/java/br/unifacisa/GatWayApplication.java
+++ GateWay/src/main/java/br/unifacisa/GatWayApplication.java
@@ -1,4 +1,4 @@
-package com.hackaton.lindoOlhar;
+package br.unifacisa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
